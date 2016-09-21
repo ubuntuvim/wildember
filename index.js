70e7a188a71ea735bb47dbb1306f22a5dacfa104
ubuntuvim <1527254027@qq.com>
2016-09-22 00:29:49 +0800

    修复bug，更新版本号为1.3.0

--- index.js
+++ index.js
@@ -2,5 +2,22 @@
 'use strict';
 
 module.exports = {
-  name: 'wildember'
+    name: 'wildember'
+
+    // included: function included(app) {
+    //   this._super.included(app);
+    //
+    //   // make sure app is correctly assigned when being used as a nested addon
+    //   if (app.app) {
+    //     app = app.app;
+    //   }
+    //   this.app = app;
+    //
+    //   this.app.import(app.bowerDirectory + '/wildember/wildember.js');
+    //
+    //   app.import('vendor/wildember/shim.js', {
+    //     type: 'vendor',
+    //     exports: { 'wildember': ['default'] }
+    //   });
+    // }
 };
