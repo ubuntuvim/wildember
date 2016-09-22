129ece30511441945528f5c9cc078c394435bb8f
ubuntuvim <1527254027@qq.com>
2016-09-23 03:13:04 +0800

    更新版本

--- blueprints/wildember/index.js
+++ blueprints/wildember/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var EOL         = require('os').EOL;
-var chalk       = require('chalk');
+// var EOL         = require('os').EOL;
+// var chalk       = require('chalk');
 
 
 module.exports = {
@@ -11,51 +11,59 @@ module.exports = {
     // to us
   },
 
-  availableOptions: [
-    { name: 'url', type: String }
-  ],
-
-  afterInstall: function(options) {
-
-    var bowerDeps = [
-      {
-        name: 'wildember',
-        target: '^0.1.0'
-      }
-    ];
-
-    return this.addBowerPackagesToProject(bowerDeps).then(function () {
-      var g = chalk.green;
-      var y = chalk.yellow;
-      var b = chalk.blue;
-      var m = chalk.magenta;
-      var r = chalk.red;
-      var out = EOL;
-
-      out += y('wildember') + ' installed.' + EOL +
-          EOL +
-          r('CONFIGURATION REQUIRED') + EOL +
-          EOL +
-          '请参考：https://github.com/ubuntuvim/WildEmber进行配置使用，或者参考下面的配置。' +
-          EOL + EOL;
-
-
-      out += "配置示例:" + EOL +
-          EOL +
-          g("// app/adapters/application.js") + EOL +
-          "" + EOL +
-          "import DS from 'ember-data';" + EOL +
-          "import WildemberAdapter from 'wild-ember/adapters/wildember';" + EOL +
-          "" + EOL +
-          "export default WildemberAdapter.extend({" + EOL +
-          "    wilddogConfig: {" + EOL +
-          "        syncDomain: 'wildember.wilddog.com'," + EOL +
-          "        syncURL: 'https://wildember.wilddogio.com'" + EOL +
-          "    }" + EOL +
-          "});" + EOL +
-          EOL;
-
-      this.ui.writeLine(out);
-    }.bind(this));
+  afterInstall: function() {
+      return this.addBowerPackageToProject('wildember'); // is a promise
   }
+
+  // availableOptions: [
+  //   { name: 'url', type: String }
+  // ],
+  //
+  // afterInstall: function(options) {
+  //
+  //   var bowerDeps = [
+  //     {
+  //       name: 'wildember',
+  //       target: '^0.1.0'
+  //     }
+  //   ];
+  //
+  //   // return this.addBowerPackagesToProject('wildember.js', '*');
+  //
+  //
+  //   return this.addBowerPackagesToProject(bowerDeps).then(function () {
+  //     var g = chalk.green;
+  //     var y = chalk.yellow;
+  //     var b = chalk.blue;
+  //     var m = chalk.magenta;
+  //     var r = chalk.red;
+  //     var out = EOL;
+  //
+  //     out += y('wildember') + ' installed.' + EOL +
+  //         EOL +
+  //         r('CONFIGURATION REQUIRED') + EOL +
+  //         EOL +
+  //         '请参考：https://github.com/ubuntuvim/WildEmber进行配置使用，或者参考下面的配置。' +
+  //         EOL + EOL;
+  //
+  //
+  //     out += "配置示例:" + EOL +
+  //         EOL +
+  //         g("// app/adapters/application.js") + EOL +
+  //         "" + EOL +
+  //         "import DS from 'ember-data';" + EOL +
+  //         "import WildemberAdapter from 'wild-ember/adapters/wildember';" + EOL +
+  //         "" + EOL +
+  //         "export default WildemberAdapter.extend({" + EOL +
+  //         "    wilddogConfig: {" + EOL +
+  //         "        syncDomain: 'wildember.wilddog.com'," + EOL +
+  //         "        syncURL: 'https://wildember.wilddogio.com'" + EOL +
+  //         "    }" + EOL +
+  //         "});" + EOL +
+  //         EOL;
+  //
+  //     this.ui.writeLine(out);
+  //   }.bind(this));
+
+  // }
 };
--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 'use strict';
 
 module.exports = {
-    name: 'wildember'
+    name: 'wildember',
 
-    // included: function included(app) {
-    //   this._super.included(app);
-    //
-    //   // make sure app is correctly assigned when being used as a nested addon
+    included: function(app, parentAddon) {
+      this._super.included(app);
+
+      // make sure app is correctly assigned when being used as a nested addon
     //   if (app.app) {
     //     app = app.app;
     //   }
     //   this.app = app;
-    //
-    //   this.app.import(app.bowerDirectory + '/wildember/wildember.js');
-    //
-    //   app.import('vendor/wildember/shim.js', {
+
+      app.import(app.bowerDirectory + '/wildember/dist/js/wildember.js');
+      //
+    //   this.app.import('vendor/wildember/shim.js', {
     //     type: 'vendor',
     //     exports: { 'wildember': ['default'] }
     //   });
-    // }
+    }
 };
